70e9e5940b1cdd48b6c970cefccafbcf7cc22d4d
Daniel Santos <a14702@oficina.pt>
2024-10-26 22:36:16 +0100

    Update Trabalho.py

--- Trabalho.py
+++ Trabalho.py
@@ -1,24 +1,25 @@
 escolha = input("Deseja jogar este jogo? (Sim/Não): ")
 if escolha == "Sim":
     print ("Bem vindo à floresta!")
+    print("O teu objetivo é encontrar a saída sem cair nas armadilhas.\n")
 else:
   print("OK, o jogo não foi iniciado")
   exit ()
-escolha = input("Há tua frente vais encontrar 2 portas a da esquerda e a da direita, qual vais escolher? (Esquerda/Direita): ")
-if escolha == "Esquerda":
-  print("Encontras-te 3 cobras!, Desejas Atacar ou Fugir para a Direita?")
-  escolha = input("(Atacar/Fugir): ")
-  if escolha == "Atacar":
-    print("Acabas-te de morrer\n |GAME OVER|")
-  else:
-    escolha == "Fugir"
-    print("Conseguis-te fugir com sucesso para a porta da Direita!")
+
+escolha = input("Estás preso, Tenta sair daí o mais rapido possivel. Escolhe 'esquerda' ou 'direita': ").lower()
+
+if escolha == "esquerda":
+    escolha = input("Encontraste um lago. Queres 'atravessar' ou 'ir á volta' do lago?: ").lower()
+
+    if escolha == "ir á volta":
+        escolha = input("Encontraste uma caverna. Queres 'entrar' na caverna ou 'seguir em frente'?: ").lower()
+
+        if escolha == "entrar":
+            print("Encontraste a saída secreta! Parabéns, escapaste da floresta!")
+        else:
+            print("Perdeste-te na floresta. GAME OVER!")
+    else:
+        print("O lago era muito fundo e não conseguiste atravessar. GAME OVER!")
+
 else:
-escolha == "Direita":
-  print("Á tua frente tens mais 3 portas a Porta Vermelha a Azul e a Preta, qual vais escolher? (Vermelha/Azul/Preta): ")
-  escolha == "Vermelha":
-    print("á tua frente tens uma espada, pega nela, mas se vires bem não consegues passar, tenta outra porta")
-  escolha == "Preta":
-    print("Tens um escudo á tua frente, mas continuas sem conseguir passar, tenta a ultima porta (Azul) ")
-  escolha == "Azul":
-    print("Tens um monstro á tua frente, como conseguiste o escudo e a espada tenta atacar o tigre: ") 
+    print("Infelizmente, caíste numa armadilha. GAME OVER!")
